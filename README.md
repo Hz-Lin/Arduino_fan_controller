e295516842aea247fd799c8f043be9437abd540c
Hz lin <huizhi.lin0@gmail.com>
2022-05-09 18:12:13 +0200

    on and off control of the fan

--- README.md
+++ README.md
@@ -4,7 +4,9 @@
 
 The aim of this project is to control several computer fans with Arduino, so that they can cool the air around a server.
 
-Arduino gets temperature data from a temperature sensor and display the information on a OLED screen.
+Arduino gets temperature data from a temperature sensor and display the information on an OLED screen.
+
+If the temperature is too high, then the Relay Module will be turned on so that the fans start spinning. Whe the temperature drops to a acceptable number, the Relay Module will be turned off so the fans will also stop.
 
 ## Arduino
 
@@ -12,7 +14,11 @@ The type of Arduino is **ESP8266 NodeMCU V3 Lolin**
 
 [ESP8266 NodeMCU Tutorial](https://diyi0t.com/esp8266-nodemcu-tutorial/)
 
-## Breadboard Power Supply Module
+## Breadboard and Power Supply Module
+
+I used a solderless breadboard for experting. If you want to make a project permanent version, you may want to transfer it to a perfboard or a printed circuit board.
+
+[Breadboarding & Prototyping for Electronics, Arduino & Raspberry Pi](https://www.youtube.com/watch?v=Y3Kx2RlLXsY&list=WL&index=7)
 
 For power the Arduino and other equipment without a computer
 
@@ -47,3 +53,15 @@ It is a 1.3 inch OLED I2C Screen.
 To use the screen, the library **U8g2** is used.
 
 [u8g2 reference](https://github.com/olikraus/u8g2/wiki/u8g2reference#setcursor)
+
+## Fan Controller
+
+I got three 12V fans from my old desktop. They have 3-pin connector, so technically I can only control the speed of them by changing the voltage supplied to it.
+
+However, I choose to use the on/off control, because I don't have a mosfet. I have a relay module which was given to me along with the old Arduino. Therefore, with the relay I can just turn the fans on when it is too hot and turn off them when it is cool enough.
+
+[How to Control Fan Speed](https://www.analog.com/en/analog-dialogue/articles/how-to-control-fan-speed.html)
+
+I used a one channel relay module.
+
+[Interface One Channel Relay Module with Arduino](https://lastminuteengineers.com/one-channel-relay-module-arduino-tutorial/)
--- afc.ino
+++ afc.ino
@@ -3,6 +3,8 @@
  * Uses a DHT11 temperature sensor, library DHT
  * Display results on a 128 x 64 OLED display
  * Use library U8g2lib for display and Wire library for I2C
+ * When the temperature is higher then the maxTemp, turn on the Relay
+ * When the temperature is lower then the minTemp, turn off the Relay
  */
 
 // Include Libraries for display
@@ -11,19 +13,26 @@
 // Include DHT library for the sensor
 #include "DHT.h"
 
+// set the pin for relay (fans)
+#define FANPIN D3
 // Set the pin for the DHT
 #define DHTPIN D5
 // Define the type of the sensor
 #define DHTTYPE DHT11
 // Initialize DHT sensor for normal 16mhz Arduino
 DHT dht(DHTPIN, DHTTYPE);
-
 // Define the display object, it represents the screen
 U8G2_SH1106_128X64_NONAME_F_HW_I2C display(U8G2_R0, U8X8_PIN_NONE);
 
-#define timeDelay 2000
+// the frequence for checking the temp
+#define timeDelay 5000
+// temp for turn the fans on
+float maxTemp = 30;
+// temp for turn the fans off
+float minTemp = 25;
 
-void display_prepare() {
+// prepare the settings for the display
+void displayPrepare() {
 display.setFont(u8g2_font_6x10_tf);
 display.setFontRefHeightExtendedText(); //for drawing characters
 display.setDrawColor(1); //1 is default, 0 means space around the character is lit up
@@ -36,39 +45,44 @@ void setup() {
   Wire.begin();
   // initialize OLED
   display.begin();
-  display_prepare();
+  displayPrepare();
   display.clearBuffer();
-  dht.begin(); //initialize sensor device
+  // Initialize sensor device
+  dht.begin();
+  // setup the pin for relay
+  pinMode(FANPIN, OUTPUT);
  }
 
 void loop() {
-  // Readings used for average
-  const int numReadings = 5;
-  // Set variables to zero
   float temp = 0;
-  float avetemp = 0;
   float hum = 0;
-  float avehum = 0;
   
   // Wait a few seconds to aloow sensor to stablize
   delay(timeDelay);
  
   display.drawStr(0, 0, "Temperatur(C):");
   display.drawStr(0, 15, "Humidity(%):");
-  
-  //  display.drawStr(0, 40, "Realtime Temp(C):");
-  for (int i = 0; i < numReadings; i++) {
-      temp += dht.readTemperature();
-      hum += dht.readHumidity();
-      delay(timeDelay);
-  }
-  // calculate the average
-  avetemp = temp / numReadings;
-  avehum = hum / numReadings;
+  display.drawStr(0, 30, "Fans status:");
+
+  temp = dht.readTemperature();
+  hum = dht.readHumidity();
   display.setCursor(100, 0);
-  display.print(avetemp, 0);
+  display.print(temp, 0);
   display.setCursor(100, 15);
-  display.print(avehum, 0);
+  display.print(hum, 0);
+  if (temp >= maxTemp) {
+    // turn on the relay
+    digitalWrite(FANPIN, LOW);
+    display.drawStr(100, 30, "ON");
+  }
+  else if (temp <= minTemp) {
+    // turn off the relay
+    digitalWrite(FANPIN, HIGH);
+    display.drawStr(100, 30, "OFF");
+  }
+  else {
+    display.drawStr(100, 30, "ON");
+  }
   display.sendBuffer();
   delay(timeDelay);
   display.clearBuffer();
